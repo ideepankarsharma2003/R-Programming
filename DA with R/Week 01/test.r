10a2a36574e4f32e90af2078c827e6e70eff0ce0
Lucky <deepankarsharma2003@gmail.com>
2022-03-18 20:28:31 +0530

    week 01

--- DA with R/Week 01/test.r
+++ DA with R/Week 01/test.r
@@ -1,1 +1,10 @@
-print("Hello world")
+# print("Hello world")
+install.packages("palmerpenguins")
+
+
+
+# View(penguins)
+
+library(ggplot2)
+
+ggplot(data= penguins, aes(x= flipper_length_mm, y= body_mass_g)) + geom_point(aes(color= species))
